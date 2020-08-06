94546108efee0a4438d59dce5a5d9497fe0fa25f
SARTHAK MODI <S-modi>
2020-08-07 00:32:53 +0530

    table 12

--- ex1.py
+++ ex1.py
@@ -4,6 +4,7 @@ print("This is table of 12")
 print("\n\n\n")
 for i in range(1,11):
     print(12*i+2*i)
-print("this is table of 14 using table of 12")    
+print("this is table of 14 using table of 12")
+print("hello")
     
     
